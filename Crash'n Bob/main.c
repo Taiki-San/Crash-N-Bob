a2557e64810d03df67efdb1ba552d52f1ca778b3
Taiki <taiki@rakshata.com>
2015-09-16 08:30:56 +0200

    Initial Commit

---
+++ Crash'n Bob/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Crash'n Bob
+//
+//  Created by Taiki on 16/09/2015.
+//  Copyright © 2015 Taiki. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
